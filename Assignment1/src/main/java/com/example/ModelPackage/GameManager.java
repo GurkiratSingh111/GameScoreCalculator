fb1c37ada72135e84326493e8275b9cf2dc8c519
Gurkirat <gsa119@sfu.ca>
2022-09-27 09:01:55 -0700

    GameManager class completed

--- Assignment1/src/main/java/com/example/ModelPackage/GameManager.java
+++ Assignment1/src/main/java/com/example/ModelPackage/GameManager.java
@@ -1,4 +1,26 @@
 package com.example.ModelPackage;
-
+import java.util.ArrayList;
 public class GameManager {
+    public ArrayList<Game> game;
+
+    public GameManager()
+    {
+
+        game= new ArrayList<>();
+    }
+    public void insert(Game g)
+    {
+
+        game.add(g);
+    }
+    public Game retrieve(int index)
+    {
+        return game.get(index);
+    }
+    public void remove(int index)
+    {
+
+        game.remove(index-1);
+    }
+
 }
